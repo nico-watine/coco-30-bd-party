7bc237fcf420958ef7f6e9814c08234244f57969
Nico Watine <nico@studioncreations.com>
2019-08-13 00:40:52 -0500

    Wrap in a grid

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,8 +8,10 @@ og-image:
 body-id: index
 body-class: index
 ---
-<section class="splash-prose">
-	<div class="inner">
-		<h1><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
-	</div>
-</section>
+<div class="grid-x grid-padding-x">
+	<section class="splash-prose">
+		<div class="inner">
+			<h1><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
+		</div>
+	</section>
+</div>
