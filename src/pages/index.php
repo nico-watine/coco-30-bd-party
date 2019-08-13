4445ddfc0c7217fd7e62d219fba23441264bac1d
Nico Watine <nico@studioncreations.com>
2019-08-13 01:00:37 -0500

    Add splash bg

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,7 +8,7 @@ og-image:
 body-id: index
 body-class: index
 ---
-<div class="grid-x align-middle" style="height: 100vh;">
+<div class="grid-x align-middle splash-landing" style="height: 100vh; ">
 	<div class="cell">
 		<section class="splash-prose" style="max-width: 680px; width: 100%; padding-left: 104px; padding-top: 48px; padding-bottom: 48px; padding-right: 16px;">
 			<h1 class="m-0"><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
