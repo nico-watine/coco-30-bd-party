734dc53f05da26b3dbc5fbe5539ca84ce0e059c2
Nico Watine <nico@studioncreations.com>
2019-08-13 00:44:39 -0500

    Add padding and h3

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,7 +9,8 @@ body-id: index
 body-class: index
 ---
 <div class="grid-x grid-padding-x">
-	<section class="splash-prose" style="padding-left: 104px; padding-top: 48px; padding-bottom: 48px;">
+	<section class="splash-prose" style="padding-left: 104px; padding-top: 48px; padding-bottom: 48px; padding-right: 16px;">
 		<h1><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
+		<h3 class="h3" style="max-width: 560px;">Come celebrate her 30th birthday with sunshine, pool time & lunch</h3>
 	</section>
 </div>
