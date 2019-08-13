8eaabadde76ee97d9531a30f254019c294f02099
Nico Watine <nico@studioncreations.com>
2019-08-13 00:50:57 -0500

    Add margins

--- src/pages/index.php
+++ src/pages/index.php
@@ -10,8 +10,8 @@ body-class: index
 ---
 <div class="grid-x grid-padding-x">
 	<section class="splash-prose" style="padding-left: 104px; padding-top: 48px; padding-bottom: 48px; padding-right: 16px;">
-		<h1><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
-		<div class="white-hr" style="height: 8px; background: #fff; width: 100%; max-width: 280px;"></div>
+		<h1 class="m-0"><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
+		<div class="white-hr mb-24" style="height: 8px; background: #fff; width: 100%; max-width: 280px;"></div>
 		<h3 class="h3" style="max-width: 560px;">Come celebrate her 30th birthday with sunshine, pool time & lunch</h3>
 	</section>
 </div>
