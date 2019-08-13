2df373c0342c2a7a2752890460b7dfdba5d22e93
Nico Watine <nico@studioncreations.com>
2019-08-13 01:16:23 -0500

    Styling button

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,13 +11,13 @@ body-class: index
 <div class="grid-x align-middle splash-landing" style="height: 100vh; ">
 	<div class="cell">
 		<section class="splash-prose" style="max-width: 680px; width: 100%;">
-			<div class="inner" style="padding-left: 56px; padding-top: 48px; padding-bottom: 48px; padding-right: 16px;">
+			<div class="inner" style="margin-bottom: 24px; padding-left: 56px; padding-top: 48px; padding-bottom: 48px; padding-right: 16px;">
 				<h1 class="m-0"><span class="h3 display-block">You are invited to</span> <span class="mb-16 h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
 				<div class="white-hr mb-32" style="height: 8px; background: #fff; width: 100%; max-width: 280px;"></div>
 				<h3 class="m-0 h3" style="max-width: 560px;">Come celebrate her 30th birthday with sunshine, pool time & lunch</h3>
 			</div>
 			<div class="text-center">
-				<a href="" class="button">RSVP</a>
+				<a href="" class="button" style="background: #E9E9E9;border: 4px solid #D59F53;border-radius: 12px;font-size: 36px;letter-spacing: 1px; color: #000;"><strong>RSVP</strong></a>
 			</div>
 		</section>
 	</div>
