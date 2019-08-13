b0e81c44de59fea7218a8e81e31fc81e97fc299c
Nico Watine <nico@studioncreations.com>
2019-08-13 00:38:46 -0500

    Create prose markup

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,8 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="splash-prose">
+	<div class="inner">
+		<h1><span class="h3 display-block">You are invited to</span> <span class="h2 display-block">Corinne’s <strong>30th</strong> birthday</span> <span class="h1-large script-font display-block">Pool Party!</span></h1>
+	</div>
+</section>
